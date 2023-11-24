1a426d3d57ec3d57f6aba5b423ef868f25129f02
sammyatale <134675667+sammyatale@users.noreply.github.com>
2023-11-24 23:41:32 +0530

    quiz.py

--- quiz.py
+++ quiz.py
@@ -21,14 +21,14 @@ else:
     print('Incorrect..!')
 
 answer = input("question:" "In programming, what does API stand for? ")
-if answer.lower() == "application arogramming anterface":
+if answer.lower() == "application Programming Interface":
     print('correct..!')
     score += 1
 else:
     print('Incorrect..!')
 
 answer = input("question :" "What is the main purpose of a firewall? ")
-if answer.lower() == "to protect a network from unauthorized access":
+if answer.lower() == "to protect network from unauthorized access":
     print('correct..!')
     score += 1
 else:
@@ -36,6 +36,62 @@ else:
 
 answer = input("question :" "Which programming language is known for its use in machine learning and data analysis? ")
 if answer.lower() == "python":
+    print('correct..!')
+    score += 1
+else:
+    print('Incorrect..!')
+
+answer = input("question :" " Which programming language is commonly used for developing mobile applications for both iOS and Android platforms? ")
+if answer.lower() == "Swift":
+    print('correct..!')
+    score += 1
+else:
+    print('Incorrect..!')
+
+answer = input("question :" " Which technology is used for wireless communication between devices over short distances, such as transferring files between smartphones? ")
+if answer.lower() == "Bluetooth":
+    print('correct..!')
+    score += 1
+else:
+    print('Incorrect..!')
+
+answer = input("question :" " In the context of cloud computing, what does the acronym 'IaaS' stand for? ")
+if answer.lower() == "Infrastructure as a Service":
+    print('correct..!')
+    score += 1
+else:
+    print('Incorrect..!')
+
+answer = input("question :" " Which company developed the programming language 'JavaScript'? ")
+if answer.lower() == "Netscape":
+    print('correct..!')
+    score += 1
+else:
+    print('Incorrect..!')
+
+answer = input("question :" " What does the term 'URL' stand for in the context of web addresses? ")
+if answer.lower() == "Uniform Resource Locator":
+    print('correct..!')
+    score += 1
+else:
+    print('Incorrect..!')
+
+answer = input("question :" " What is the function of an accelerometer in a smartphone or tablet? ")
+if answer.lower() == "Detect motion and orientation":
+    print('correct..!')
+    score += 1
+else:
+    print('Incorrect..!')
+
+answer = input("question :" " What does OOP stand for ? ")
+if answer.lower() == "Object-oriented programming":
+    print('correct..!')
+    score += 1
+else:
+    print('Incorrect..!')
+
+answer = input("question :" " What does OOP stand for ? ")
+if answer.lower() == "Object-oriented programming":
     print('correct..!')
     score += 1
 else:
